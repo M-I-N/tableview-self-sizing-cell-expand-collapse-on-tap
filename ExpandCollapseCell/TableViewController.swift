26fb9ac73194ade825bcbf661eeaca102e649da2
Mufakkharul Islam Nayem <nil.nayem@gmail.com>
2020-02-17 22:36:11 +0600

    Replace initial view controller with table view controller

--- ExpandCollapseCell/TableViewController.swift
+++ ExpandCollapseCell/TableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TableViewController.swift
 //  ExpandCollapseCell
 //
 //  Created by Mufakkharul Islam Nayem on 2/17/20.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
